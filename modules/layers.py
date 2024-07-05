8bdd7b42d79b9f470cdc79f786edb6b23234e1a7
Iris Marmouset <irismarmouset@gmail.com>
2024-07-05 13:09:17 -0700

    Started the autoencoder

--- modules/layers.py
+++ modules/layers.py
@@ -6,72 +6,66 @@ from torch.nn.parameter import Parameter
 import math
 
 
-class PyrGlobal(nn.Module):
-     def __init__(self, hidden_dim, output_dim):
-        super(PyrGlobal, self).__init__()
-        self.hidden_dim = hidden_dim # SST top_down input
-        self.output_dim=output_dim # Pyr classification of the image
 
-        self.fc1 = nn.Linear(hidden_dim, output_dim)
-        self.activation = nn.Sigmoid()
+
+class PV(nn.Module):
+    # Autoencoder
+
+    def __init__(self, input_dim, encoded_dim):
+        super().__init__()
+        self.input_dim = input_dim
+        self.encoded_dim = encoded_dim
+
+        self.Thal2PV = Thal2PV(input_dim, encoded_dim) # encoder
+        self.PV2Pyr = PV2Pyr(encoded_dim, input_dim) # decoder
 
         self.reset_parameters()
 
-        self.hook = {'fc1': []}
-        self.register_hook = False
-                   
-     def forward(self, input):
-        output = self.activation(self.fc1(input))
+    def reset_parameters(self):
+        for layer in [self.Thal2PV, self.PV2Pyr]:
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
 
-        if self.register_hook:
-            output.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
+    def forward(self, input):
+        encoded = self.Thal2PV(input)
+        decoded = self.PV2Pyr(encoded)
+        # decoded.view(-1, 1, 28, 28)
 
-        return output
-
-     def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.fc1.weight.size(1))
-        self.fc1.weight.data.uniform_(-stdv, stdv)
-        self.fc1.bias.data.uniform_(-stdv, stdv)
-
-     def hook_fn(self, grad, name):
-        self.hook[name].append(grad)
-
-     def reset_hook(self):
-        self.hook = {'fc1': []}
-    
-
-
+        return encoded, decoded
 
 
 class SST(nn.Module):
-    def __init__(self, input_dim, topdown_dim):
-        super(SST, self).__init__()
-        self.input_dim = input_dim # (?) input
-        self.hidden_dim = topdown_dim # sent to Pyr neurons as top-down input
+    # Simple classifier for a global loss computation
 
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.SST_input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_dim)
         self.flatten = Flatten()
-        self.fc1 = nn.Linear(input_dim, input_dim)
-        self.fc2 = nn.Linear(input_dim, input_dim)
-        self.fc3 = nn.Linear(input_dim, topdown_dim)
         self.activation = nn.Sigmoid()
-            
+
         self.hook = {'fc1': [], 'fc2': [], 'fc3': []}
         self.register_hook = False
 
         self.reset_parameters()
 
+
     def forward(self, input):
         input = self.flatten(input)
         fc1_out = self.activation(self.fc1(input))
         fc2_out = self.activation(self.fc2(fc1_out))
-        top_down = self.activation(self.fc3(fc2_out))
+        output = self.fc3(fc2_out)
 
         if self.register_hook:
             fc1_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
             fc2_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc2'))
-            top_down.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc3'))
+            output.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc3'))
 
-        return top_down
+        return output
 
     def reset_parameters(self):
         for layer in [self.fc1, self.fc2, self.fc3]:
@@ -84,7 +78,107 @@ class SST(nn.Module):
         self.hook[name].append(grad)
 
     def reset_hook(self):
-        self.hook = {'fc1': [], 'fc2': [], 'fc3': []}    
+        self.hook = {'fc1': [], 'fc2': [], 'fc3': []} 
+
+
+
+
+
+
+
+class Thal2PV(nn.Module):
+    # Encoder part of the autoencoder
+
+    def __init__(self, input_dim, encoded_dim):
+        super().__init__()
+        self.input_dim = input_dim
+        self.encoded_dim = encoded_dim
+
+        self.flatten = Flatten()
+        self.activation = nn.Sigmoid()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, encoded_dim)
+
+        self.hook = {'fc1': [], 'fc2': [], 'fc3': []}
+        self.register_hook = False
+
+        self.reset_parameters()
+
+
+    def forward(self, input):
+        input = self.flatten(input)
+        fc1_out = self.activation(self.fc1(input))
+        fc2_out = self.activation(self.fc2(fc1_out))
+        encoded = self.activation(self.fc3(fc2_out))
+
+        if self.register_hook:
+            fc1_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
+            fc2_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc2'))
+            encoded.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc3'))
+
+        return encoded
+
+    def reset_parameters(self):
+        for layer in [self.fc1, self.fc2, self.fc3]:
+            if type(layer) == nn.Linear:
+                stdv = 1. / math.sqrt(layer.weight.size(1))
+                layer.weight.data.uniform_(-stdv, stdv)
+                layer.bias.data.uniform_(-stdv, stdv)
+
+    def hook_fn(self, grad, name):
+        self.hook[name].append(grad)
+
+    def reset_hook(self):
+        self.hook = {'fc1': [], 'fc2': [], 'fc3': []} 
+
+
+
+
+class PV2Pyr(nn.Module):
+    # Decoder part of the autoencoder
+
+    def __init__(self, encoded_dim, decoded_dim):
+        super().__init__()
+        self.encoded_dim = encoded_dim
+        self.decoded_dim = decoded_dim
+
+        self.activation = nn.Sigmoid()
+        self.fc1 = nn.Linear(encoded_dim, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, decoded_dim)
+
+        self.hook = {'fc1': [], 'fc2': [], 'fc3': []}
+        self.register_hook = False
+
+        self.reset_parameters()
+
+
+    def forward(self, input):
+        fc1_out = self.activation(self.fc1(input))
+        fc2_out = self.activation(self.fc2(fc1_out))
+        decoded = self.activation(self.fc3(fc2_out))
+
+        if self.register_hook:
+            fc1_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
+            fc2_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc2'))
+            decoded.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc3'))
+
+        return decoded
+
+    def reset_parameters(self):
+        for layer in [self.fc1, self.fc2, self.fc3]:
+            if type(layer) == nn.Linear:
+                stdv = 1. / math.sqrt(layer.weight.size(1))
+                layer.weight.data.uniform_(-stdv, stdv)
+                layer.bias.data.uniform_(-stdv, stdv)
+
+    def hook_fn(self, grad, name):
+        self.hook[name].append(grad)
+
+    def reset_hook(self):
+        self.hook = {'fc1': [], 'fc2': [], 'fc3': []} 
+
 
 
 
@@ -98,216 +192,18 @@ class Flatten(nn.Module):
 
 
 
-class PV(nn.Module):
-    def __init__(self, input_dim, latent_dim, feedback_alignment = False):
-        super().__init__()
-        self.input_dim = input_dim # thalamic input
-        self.latent_dim = latent_dim # PV prediction of Pyr cells 
 
-        self.flatten = Flatten()
-        self.fc1 = nn.Linear(input_dim, input_dim)
-        self.activation = nn.Sigmoid()
-        if feedback_alignment:
-            self.fc2 = LinearFA(input_dim, latent_dim)
-        else: 
-            self.fc2 = nn.Linear(input_dim, latent_dim)
-            
-        self.hook = {'fc1': [], 'fc2': []}
-        self.register_hook = False
-        self.reset_parameters()
 
-        # self.PV_opto_mask = None
-        # self.PV_scale = 0
 
-    def forward(self, input):
-        input = self.flatten(input)
-        PV_out  = self.activation(self.fc1(input))
-        PV_pred = self.activation(self.fc2(PV_out))
 
-        if self.register_hook:
-            PV_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
-            PV_pred.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc2'))
-
-        return PV_out, PV_pred
     
-    def reset_parameters(self):
-        for layer in [self.fc1, self.fc2]:
-            if type(layer) == nn.Linear:
-                stdv = 1. / math.sqrt(layer.weight.size(1))
-                layer.weight.data.uniform_(-stdv, stdv)
-                layer.bias.data.uniform_(-stdv, stdv)
-
-    def hook_fn(self, grad, name):
-        self.hook[name].append(grad)
-
-    def reset_hook(self):
-        self.hook = {'fc1': [], 'fc2': []}       
-
-
-class PyrLocal(nn.Module):
-    def __init__(self, input_dim, latent_dim, PV_modulation_factor=0.3):
-        super().__init__()
-        self.input_dim = input_dim # Thalamus input
-        self.latent_dim=latent_dim # PV prediction of Pyr cells, output of fc1, and Pyramidal output
-        self.PV_modulation_factor = PV_modulation_factor # How much the PV prediction modulates the Pyr output
-
-        self.flatten = Flatten()
-        self.fc1 = nn.Linear(input_dim, latent_dim)
-        self.activation = nn.Sigmoid()
-
-        self.reset_parameters()
-
-        self.hook = {'fc1': []}
-        self.register_hook = False
-
-    def forward(self, input, PV_pred):
-        input = self.flatten(input)
-        Pyr_out = self.activation(self.fc1(input) + self.PV_modulation_factor*PV_pred)
-        #Pyr_out = self.activation(self.fc1(input) + self.PV_modulation_factor*PV_pred.detach())
-        # detach: no gradients will be computed on PV_pred during backprop, feedback alignment to do done on PV class 
-
-        if self.register_hook:
-            Pyr_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
-
-        return Pyr_out
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.fc1.weight.size(1))
-        self.fc1.weight.data.uniform_(-stdv, stdv)
-        self.fc1.bias.data.uniform_(-stdv, stdv)
-
-    def hook_fn(self, grad, name):
-        self.hook[name].append(grad)
-
-    def reset_hook(self):
-        self.hook = {'fc1': []}
-
-
-        
-class Decoder(nn.Module):
-    def __init__(self, latent_dim,  input_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(latent_dim, input_dim)
-        self.reset_parameters()
-
-    def forward(self, x):
-        self.e = self.fc1(x)
-        return self.e
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.fc1.weight.size(1))
-        self.fc1.weight.data.uniform_(-stdv, stdv)
-        self.fc1.bias.data.uniform_(-stdv, stdv)
 
 
 
-class PyrCombined(nn.Module):
-    def __init__(self, thal_input_dim, latent_dim, topdown_dim, output_dim, PV_modulation_factor=0.3):
-        super().__init__()
-        self.thal_input_dim = thal_input_dim # Thalamus input to PV and Pyr
-        self.latent_dim=latent_dim # Dim of PV prediction of Pyr cells; of output of fc1; of Pyramidal output
-        self.topdown_dim = topdown_dim # SST output (top_down) input to Pyr neurons
-        self.output_dim=output_dim # Pyr classification of the image      
-        self.PV_modulation_factor = PV_modulation_factor # How much the PV prediction modulates the Pyr output
-
-        self.activation = nn.Sigmoid()
-        self.flatten = Flatten()
-        self.fc1 = nn.Linear(topdown_dim, thal_input_dim)
-        self.fc2 = nn.Linear(thal_input_dim, latent_dim)
-        self.fc3 = nn.Linear(latent_dim, latent_dim)
-        self.fc4 = nn.Linear(latent_dim, output_dim)
-
-        self.reset_parameters()
-
-        self.hook = {'fc1': [], 'fc2': [], 'fc3': []}
-        self.register_hook = False
-
-    def forward(self, input_thal, PV_pred, top_down):
-        # 1 - Top-down inputs into apical dendrites
-        td_processed = self.activation(self.fc1(top_down))
-        # 2 - Thalamic inputs into basal dendrites, sumed to the top-down processed
-        input_thal = self.flatten(input_thal)
-        td_thal_processed = self.activation(self.fc2(input_thal + td_processed))
-        # 3 - PV_pred input into basal dendrites, sumed to thalamic and top-down inputs
-        PV_input = self.PV_modulation_factor * PV_pred
-        Pyr_pred = self.activation(self.fc3(td_thal_processed + PV_input))
-        # 4 - Classification layer: Pyr_out is 10-dim and classifies the input received
-        Pyr_out = self.activation(self.fc4(Pyr_pred))
-
-        if self.register_hook:
-            td_processed.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc1'))
-            td_thal_processed.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc2'))
-            Pyr_out.register_hook(lambda grad: self.hook_fn(grad=grad,name='fc3'))
-
-        # returns a classification of the image 
-        return Pyr_pred, Pyr_out
-
-    def reset_parameters(self):
-        pass 
-
-    def hook_fn(self, grad, name):
-        self.hook[name].append(grad)
-
-    def reset_hook(self):
-        self.hook = {'fc1': [], 'fc2': [], 'fc3': []}
 
 
 
-class LinearFunctionFA(torch.autograd.Function):
-    # Taken from neoSSL shallow_mlp.py
-    @staticmethod
-    def forward(ctx, input, weight, bias=None, backward_weight=None, weight_masks=None):
-        ctx.save_for_backward(input, weight, bias, backward_weight, weight_masks)
-        output = input.mm(weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, weight, bias, backward_weight, weight_masks = ctx.saved_tensors
-        backward_weight = (backward_weight * weight_masks).to(grad_output.device)
-        input = input.to(grad_output.device)
-        grad_input = grad_output.mm(backward_weight) # use backward_weight here instead of weight.t()
-        grad_weight = grad_output.t().mm(input)
-        grad_bias = grad_output.sum(0) if bias is not None else None
-        return grad_input, grad_weight, grad_bias, None, None
-
-
-class LinearFA(nn.Module):
-    # Taken from neoSSL shallow_mlp.py
-    def __init__(self, input_features, output_features, bias=False, fa_sparsity=0.):
-        super(LinearFA, self).__init__()
-        self.input_features = input_features
-        self.output_features = output_features
-
-        # This registers the parameter with the module, making it appear in methods like `.parameters()` and `.to()`
-        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(output_features))
-        else:
-            self.register_parameter('bias', None)
-
-        # Initialize random fixed backward weights
-        self.backward_weight = torch.randn(output_features, input_features)
-        stdv = 1. / math.sqrt(self.backward_weight.size(1))
-        self.backward_weight.data.uniform_(-stdv, stdv)
-
-        
-        # Initialize forward weights and biases with the usual initialization method
-        stdv_weight = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv_weight, stdv_weight)
-        if bias:
-            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in)
-            nn.init.uniform_(self.bias, -bound, bound)
-
-        self.weight_masks = (torch.rand_like(self.backward_weight) > fa_sparsity).float()
-        self.backward_weight = self.backward_weight * self.weight_masks
-
-
-    def forward(self, input):
-        return LinearFunctionFA.apply(input, self.weight, self.bias, self.backward_weight, self.weight_masks)
 
 
 
--- modules/network.py
+++ modules/network.py
@@ -4,114 +4,38 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import math
 
-from modules.layers import Decoder, PyrLocal, PV, SST, PyrGlobal, Flatten, PyrCombined
-
-
-class GlobalNetwork(nn.Module):
-    def __init__(self, input_dim, topdown_dim, output_dim):
-        super().__init__()
-        self.input_dim = input_dim
-        self.topdown_dim = topdown_dim
-        self.output_dim = output_dim
-        
-        self.SST = SST(input_dim, topdown_dim)
-        self.PyrGlobal = PyrGlobal(topdown_dim, output_dim) 
+from modules.layers import PV, SST
 
 
-        self._initialise_weights() 
-    
-    def _initialise_weights(self):
-        for layer in [self.SST, self.PyrGlobal]:
-            if hasattr(layer, 'reset_parameters'):
-                layer.reset_parameters()
-
-    def forward(self, input):
-
-        top_down = self.SST(input) # some input processed as a top-down input sent to pyramidal neurons
-        output = self.PyrGlobal(top_down) # pryamidal neurons processing of the top-down input
-
-        return top_down, output
 
 
-    def reset_hook(self):
-        self.SST.reset_hook()
-        self.PyrGlobal.reset_hook()
-    
-    def register_hook(self, register=True):
-        self.SST.register_hook = register
-        self.PyrGlobal.register_hook = register
-    
-    
-    
+class PV_SST_Pyr(nn.Module):
+    # Network recaptitulating the PV autoencoder modalities + SST top-down modulation
 
-
-class LocalNetwork(nn.Module):
-    def __init__(self, input_dim, latent_dim, PV_modulation_factor=0.3):
-        super().__init__()
-        self.input_dim = input_dim
-        self.latent_dim = latent_dim
-        
-        self.PV = PV(input_dim, latent_dim)
-        self.Pyr = PyrLocal(input_dim, latent_dim, PV_modulation_factor) 
-        self.Decoder = Decoder(latent_dim, input_dim)
-
-        self._initialise_weights() 
-    
-    def _initialise_weights(self):
-        for layer in [self.PV, self.Pyr, self.Decoder]:
-            if hasattr(layer, 'reset_parameters'):
-                layer.reset_parameters()
-
-
-    def forward(self, inputPV, inputPyr):
-
-        PV_out, PV_pred = self.PV(inputPV) # thalamic input onto PV cells: PV output (1 layer activated) and PV predictions (2nd layer)
-        Pyr_out = self.Pyr(inputPyr, PV_pred) # input is thalamic, and Pyr_pred is the last output of the PV layer
-        #Pyr_out = self.Pyr(input2, PV_pred.detach())
-        recon = self.Decoder(Pyr_out) # reconstruct the input from the Pyr_out --> used for teaching signal 
-
-        return PV_out, PV_pred, Pyr_out, recon
-
-
-    def reset_hook(self):
-        self.PV.reset_hook()
-        self.Pyr.reset_hook()
-    
-    def register_hook(self, register=True):
-        self.PV.register_hook = register
-        self.Pyr.register_hook = register
-        
-
-
-class CombinedNetwork(nn.Module):
-    def __init__(self, thal_input_dim, td_input_dim, latent_dim, topdown_dim, output_dim, PV_modulation_factor=0.3):
+    def __init__(self, thal_input_dim, SST_input_dim, encoded_dim, output_dim=10):
         super().__init__()
         self.thal_input_dim = thal_input_dim
-        self.td_input_dim = td_input_dim
-        self.latent_dim = latent_dim
-        self.topdown_dim = topdown_dim
+        self.SST_input_dim = SST_input_dim
+        self.encoded_dim = encoded_dim
         self.output_dim = output_dim
 
-        self.PV = PV(thal_input_dim, latent_dim)
-        self.SST = SST(td_input_dim, topdown_dim)
-        self.Pyr = PyrCombined(thal_input_dim, latent_dim, topdown_dim, output_dim, PV_modulation_factor) 
-        self.Decoder = Decoder(output_dim, thal_input_dim)
+        self.PV = PV(thal_input_dim, encoded_dim)
+        self.SST = SST(SST_input_dim, output_dim)
 
         self.initialise_weights()
 
     def initialise_weights(self):
-        for layer in [self.PV, self.SST, self.Pyr, self.Decoder]:
+        for layer in [self.PV, self.SST]:
             if hasattr(layer, 'reset_parameters'):
                 layer.reset_parameters()
 
-    def forward(self, inputSST, inputPV, inputPyr):
-        top_down = self.SST(inputSST) 
-        PV_out, PV_pred = self.PV(inputPV)
-        Pyr_pred, Pyr_out = self.Pyr(inputPyr, PV_pred, top_down)
-        recon = self.Decoder(Pyr_out) 
+    def forward(self, thal_input, SST_input):
+        encoded, decoded = self.PV(thal_input)
+        output = self.SST(SST_input)
 
-        return top_down, PV_out, PV_pred, Pyr_pred, Pyr_out, recon
+        return encoded, decoded, output
 
 
 
@@ -120,20 +44,9 @@ class CombinedNetwork(nn.Module):
 
 
 
-class Net(nn.Module): # works 
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
+
+
+
+
+
