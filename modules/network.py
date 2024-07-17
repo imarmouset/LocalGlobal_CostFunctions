8491eb56a46480d8148f50376d3de6b635f4884a
Iris Marmouset <irismarmouset@gmail.com>
2024-07-16 19:50:59 -0700

    Started delusion and apathy

--- modules/network.py
+++ modules/network.py
@@ -12,25 +12,24 @@ from modules.layers import encoder, decoder, classifier
 
 
 class PV_SST_Pyr(nn.Module):
-    # Network recaptitulating the PV autoencoder modalities + SST top-down modulation
+    # Network recaptitulating the PV autoencoder modalities 
 
     def __init__(self, thal_input_dim, latent_dim, output_dim=10, 
-                 freeze_encoder=False, freeze_decoder=False, freeze_classifier=False):
+                 freeze=None, defreeze=None, swap_digits=None):
         super().__init__()
         self.thal_input_dim = thal_input_dim
         self.latent_dim = latent_dim
         self.output_dim = output_dim
+        self.swap_digits = swap_digits
 
         self.encoder = encoder(thal_input_dim, latent_dim)
         self.decoder = decoder(latent_dim, thal_input_dim)
         self.classifier = classifier(latent_dim, output_dim)
 
-        if freeze_encoder:
-            self.freeze_module('encoder')
-        if freeze_decoder:
-            self.freeze_module('decoder')
-        if freeze_classifier:
-            self.freeze_module('classifier')
+        if freeze is not None:
+            self.freeze_module(freeze)
+        if defreeze is not None:
+            self.defreeze_module(defreeze)
 
         self.initialise_weights()
 
@@ -46,27 +45,22 @@ class PV_SST_Pyr(nn.Module):
         return encoded, recon, output
     
     def freeze_module(self, module_name):
-        if module_name.lower() == 'encoder':
-            for param in self.encoder.parameters():
+        if module_name in ['encoder', 'decoder', 'classifier']:
+            for param in getattr(self, module_name).parameters():
                 param.requires_grad = False
-        elif module_name.lower() == 'decoder':
-            for param in self.decoder.parameters():
-                param.requires_grad = False
-        elif module_name.lower() == 'classifier':
-            for param in self.classifier.parameters():
-                param.requires_grad = False
+            print(f"{module_name} has been frozen.")
+        else:
+            raise ValueError(f"Invalid module name: {module_name}. "
+                             f"Must be 'encoder', 'decoder', or 'classifier'.")
 
     def unfreeze_module(self, module_name):
-        if module_name.lower() == 'encoder':
-            for param in self.encoder.parameters():
+        if module_name in ['encoder', 'decoder', 'classifier']:
+            for param in getattr(self, module_name).parameters():
                 param.requires_grad = True
-        elif module_name.lower() == 'decoder':
-            for param in self.decoder.parameters():
-                param.requires_grad = True
-        elif module_name.lower() == 'classifier':
-            for param in self.classifier.parameters():
-                param.requires_grad = True
-
+            print(f"{module_name} has been unfrozen.")
+        else:
+            raise ValueError(f"Invalid module name: {module_name}. "
+                             f"Must be 'encoder', 'decoder', or 'classifier'.")
 
 
 
--- modules/utils.py
+++ modules/utils.py
@@ -2,14 +2,18 @@ from skimage.metrics import structural_similarity as ssim
 import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+
 
 
 
 def compute_losses(recon, thal_input, output, t, loss_fn, alpha):
     recon_loss = loss_fn(recon, thal_input.view(thal_input.size(0), -1)) 
     global_loss = loss_fn(output, t)
-    #total_loss = alpha*recon_loss + (1 - alpha)*global_loss
-    total_loss = loss_fn(output, output)
+    total_loss = alpha*recon_loss + (1 - alpha)*global_loss
+    #total_loss = loss_fn(output, output)
 
     return recon_loss, global_loss, total_loss
 
@@ -64,7 +68,7 @@ def calculate_accuracy(targets, outputs, total, correct):
 
 
 def visualize_tsne(encoded_outputs, labels, epoch=None):
-    n_samples = 1000  # or however many you want
+    n_samples = 1000  # otherwise takes too much time to use all the points
     indices = np.random.choice(encoded_outputs.shape[0], n_samples, replace=False)
     encoded_outputs = encoded_outputs[indices]
     labels = labels[indices]
@@ -80,6 +84,82 @@ def visualize_tsne(encoded_outputs, labels, epoch=None):
         plt.savefig(f'./Plots/tSNE/tsne_epoch_{epoch}.png')
     else:
         plt.title('t-SNE visualization of encoded outputs - Epoch 0')
-        plt.savefig('./Plots/tSNE/tsne_epoch_0.png')
-    
+        plt.savefig('./Plots/tSNE/tsne_epoch_0.png')   
     plt.close()
+
+
+def mix_error_signals(model, target, t, recon, thal_input, output, alpha):
+
+    # Which digits of the batch are concerned? How many digits? 
+    digit_x, digit_y = model.swap_digits
+    mask_x = (target == digit_x)
+    mask_y = (target == digit_y)
+    num_mixs = min(mask_x.sum(), mask_y.sum())
+    print(f"Number of digit {digit_x}: {mask_x.sum()}; number of digit {digit_y}: {mask_y.sum()}; should be {num_mixs} exchanges")
+
+    # Selecting the outputs & their target for digit_x and digit_y
+    output_x = output[mask_x]
+    output_y = output[mask_y]
+    target_x = t[mask_x]
+    target_y = t[mask_y] 
+
+    # Compute loss of digits_x and digits_y seperately before mixing
+    loss_fn = nn.MSELoss(reduction='none')
+    loss_x = loss_fn(output_x, target_x)
+    loss_y = loss_fn(output_y, target_y)
+
+    # Mixing the losses for num_mixs units, and concatenate with the losses that have not been mixed
+    swapped_loss_x = loss_y[:num_mixs]
+    swapped_loss_y = loss_x[:num_mixs]
+    final_loss_x = torch.cat([swapped_loss_x, loss_x[num_mixs:]])
+    final_loss_y = torch.cat([swapped_loss_y, loss_y[num_mixs:]])
+  
+
+    # Computing the rest of the global loss and inserting the mixed losses
+    no_mix_global_loss = loss_fn(output, t)
+    print(f"No mix Global loss shape: {no_mix_global_loss.shape}")
+    print(f"No mix Global loss: {no_mix_global_loss}")
+    global_loss = loss_fn(output, t)
+    print(f"Global loss shape: {global_loss.shape}")
+    global_loss[mask_x] = final_loss_x
+    global_loss[mask_y] = final_loss_y
+    print(f"Global loss: {global_loss}")
+
+    # Check the differences between global_loss and no_mix_global_loss
+ 
+    print(f"{(global_loss != no_mix_global_loss).sum()} differences between global_loss and no_mix_global_loss")
+
+    # Compute the other losses normaly
+    recon_loss = loss_fn(recon, thal_input.view(thal_input.size(0), -1))
+    total_loss = alpha*recon_loss + (1 - alpha)*global_loss
+
+    return recon_loss, global_loss, total_loss
+
+
+
+def swap_digits(model, target, t, recon, thal_input, output, loss_fn, alpha):
+    # Which digits of tha batch are concerned? How many digits?
+    digit_x, digit_y = model.swap_digits
+    mask_x = (target == digit_x)
+    mask_y = (target == digit_y)
+    num_x = mask_x.sum()
+    num_y = mask_y.sum()
+    print(f"Number of digit {digit_x}: {num_x}; number of digit {digit_y}: {num_y}; should be {num_x + num_y} modifications")
+
+    # Modify the label of digit X by digit Y and vice versa
+    t_swapped = t.clone()
+    t_swapped[mask_x] = digit_y
+    t_swapped[mask_y] = digit_x
+
+    print(f"Number of differences t vs t_swapped: {(t != t_swapped).sum()}. Equal to theoretical {num_x + num_y}? ")
+
+
+    # Compute loss with swapped target for classification task ONLY
+    recon_loss, _, _ = compute_losses(recon, thal_input, output, t, loss_fn, alpha)
+    _, global_loss, _ = compute_losses(recon, thal_input, output, t_swapped, loss_fn, alpha)
+    total_loss = alpha*recon_loss + (1 - alpha)*global_loss
+
+    #print(f"Original global loss: {compute_losses(recon, thal_input, output, t, loss_fn, alpha)[1]}")
+    #print(f"Swapped global loss: {global_loss}")    
+
+    return recon_loss, global_loss, total_loss
